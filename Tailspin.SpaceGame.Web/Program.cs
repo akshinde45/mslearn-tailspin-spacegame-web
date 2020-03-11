2386a1c2e747393069692039455217314a4616c7
Akshay Shinde <akshayshinde45.as@gmail.com>
2020-03-11 11:36:00 +0530

    commented

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -7,6 +7,7 @@ namespace TailSpin.SpaceGame.Web
     {
         public static void Main(string[] args)
         {
+            //hrlo adv f
             CreateWebHostBuilder(args).Build().Run();
         }
 
